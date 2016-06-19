6c4e730f726fe8b38760127bb405621155aa5fe7
touhouota <touhouota@gmail.com>
2016-06-19 22:40:20 +0900

    タスク名などの再入力フォームからフォーカスが外れた時、サーバへ送るように変更

--- js/send.js
+++ js/send.js
@@ -1,4 +1,4 @@
-// クリックで選択している要素のid保持
+// クリックで選択している要素のid保A持
 var node_id = 0;
 // statusの文句配列
 var status_array = ["未着手...", "進行中", "終了！"];
@@ -10,26 +10,35 @@ window.onload = function(){
     target.addEventListener("dblclick", double_click, false);
     // 読み込み時に、最上位ノードを表示
     id = location.search.substr(4);
-    console.log(id);
+    var tmp = decodeURIComponent(document.cookie);
+    console.log(document.cookie);
     document.getElementsByClassName("user_name")[0].innerHTML = id;
     send(id, "view", 0);
     document.forms.controll_form.pos.value = "0";
+    var time = 1000 * 60; // ミリ秒を分へ変換
+    setInterval(repetition, time * 2);
 }
 
 // クリックした要素の色を変える
 function listener(ev){
-    if(ev.target.id){
-	console.log("first:" + node_id);
-	if(node_id > 0){
-	    document.getElementById(node_id).style.color = "";
+    console.log(ev.target.parentElement);
+    if(ev.target.parentElement.tagName.toLowerCase() != 'form'){
+	// クリックしたものが、フォームでないときは要素の選択をしてる時
+	if(ev.target.id){
+	    console.log("first:" + node_id);
+	    if(node_id > 0){
+		document.getElementById(node_id).style.color = "";
+	    }
+	    node_id = ev.target.id;
+	    document.getElementById(node_id).style.color = "red";
+	}else{
+	    if(node_id > 0){
+		document.getElementById(node_id).style.color = "";
+	    }
+	    node_id = 0;
 	}
-	node_id = ev.target.id;
-	document.getElementById(node_id).style.color = "red";
     }else{
-	if(node_id > 0){
-	    document.getElementById(node_id).style.color = "";
-	}
-	node_id = 0;
+	// クリックしたものがフォームの時は、タスク名を変更している時
     }
     console.log("end: " + node_id);
 }
@@ -43,7 +52,8 @@ function double_click(ev){
 	    inputs.push(document.createElement('input'));
 	}
 	inputs.push(document.createElement("select"));
-	var content = click_pos.innerHTML.split(',');
+	var content = click_pos.innerHTML.replace(/\r?\n/g, "").split(',');
+	console.log(content);
 
 	// タスク名
 	inputs[0].name = "add";
@@ -55,29 +65,43 @@ function double_click(ev){
 	inputs[1].value = content[1];
 
 	// 進捗部分(セレクタにする)
-	inputs[2].id="status"
+	inputs[2].id="status";
+	inputs[2].name = "status";
+	inputs[2].onblur = check_blur;
 	for(var i in status_array){
 	    var node = document.createElement('option');
 	    node.innerHTML = status_array[i];
+	    node.id = i;
+	    if(status_array[i] == content[2]){
+		node.selected = "selected";
+	    }
 	    inputs[2].appendChild(node);
 	}
+	var form = document.createElement('form');
+	form.id = click_pos.id;
+	form.className = click_pos.className;
+	parent.insertBefore(form, click_pos);
 
 	for(var i in inputs){
-	    console.log(inputs[i]);
-	    parent.insertBefore(inputs[i], click_pos);
+	    inputs[i].onblur = check_blur;
+	    form.appendChild(inputs[i]);
 	}
-	click_pos.style = "visibility: hidden;";
-    }else{
-	alert("ミス");
+	parent.removeChild(click_pos);
     }
 }
 
+// 一定時間、繰り返す内容をここに
+function repetition(){
+    var pos = document.forms.controll_form.pos;
+    console.log("グループメンバーの進捗確認するよ");
+    send(id, 'group', pos);
+}
+
 function create_request(){
-    var request = false;
     if(window.XMLHttpRequest){
-	request = new XMLHttpRequest();
+	return new XMLHttpRequest();
     }
-    return request;
+    return false;
 }
 
 // フォームから情報をとってsendする
@@ -101,7 +125,7 @@ function get_form(cmd){
 }
 
 // サーバへ要求を送る
-function send(user_id, cmd, pos, add_name, deadline){
+function send(user_id, cmd, pos, add_name, deadline, status){
     var request = create_request();
     var json = "";
     var url = "?id=" + user_id + "&cmd=" + cmd + "&pos=" + pos;
@@ -110,6 +134,8 @@ function send(user_id, cmd, pos, add_name, deadline){
     case 'add':
 	url +=  "&add_name=" + add_name + "&addel_pos=" + node_id  + "&add_dead=" + deadline;
 	break;
+    case 'update':
+	url +=  "&add_name=" + add_name + "&addel_pos=" + node_id  + "&add_dead=" + deadline + "&status=" + status;
     case 'del':
     case 'upgrade':
 	url += "&addel_pos=" + node_id;
@@ -130,7 +156,13 @@ function send(user_id, cmd, pos, add_name, deadline){
 		if(res != ''){
 		    json = JSON.parse(res);
 		    console.log(json);
-		    document.getElementsByClassName("task_list")[0].innerHTML = rewrite_page(json, pos, user_id);
+		    if(cmd == 'group'){
+			// グループを表示する時は、こっち
+			document.getElementById('group').innerHTML = rewrite_page(json,pos,user_id);
+		    }else{
+			// それ以外は、自分のところに出力
+			document.getElementById("task_list").innerHTML = rewrite_page(json, pos, user_id);
+		    }
 		    console.log(node_id);
 		}else{
 		    alert("データの追加/削除/更新に失敗しました");
@@ -139,7 +171,6 @@ function send(user_id, cmd, pos, add_name, deadline){
 	}
 	request.send(null);
 	node_id = 0;
-	
     }
 }
 
@@ -149,12 +180,12 @@ function rewrite_page(json, pos, user_id){
     var content = "";
     for(var num in json){
 	var node = json[num].node;
-	console.log(node);
+	//console.log(node);
 	content += "<li>\n";
-	content += '<span id=' + node.node_id + '>' + "\n";
+	content += '<span id="' + node.node_id + '" class="status' + node.status  + '">' + "\n";
 	content += node.task_name + ",";
-	content += node.deadline + ",";
-	content += status_array[node.status] + "\n";
+	content += node.deadline;
+	content += "," + status_array[node.status] + "\n";
 	content += "</span>\n";
 	if(json[num].child.length != 0){
 	    content += rewrite_page(json[num].child);
@@ -164,15 +195,15 @@ function rewrite_page(json, pos, user_id){
     return "<ol>\n" + content + "</ol>\n";
 }
 
-// ダブルクリックされた時、formを作って名前の変更を行う
-var prevElement = null;
-function check_blur(obj){
-    var now = obj.parentElement;
-    console.log(now);
-    if(prevElement != now){
-	confirm("別のもの");
-    }else{
-	confirm("同じもの");
-    }
-    prevElement = now;
+// フォーカスが外れた時
+function check_blur(){
+    var form = document.getElementById(node_id);
+    //console.log(form);
+    var add = form.add.value;
+    var deadline = form.date.value;
+    var pos = document.forms.controll_form.pos.value;
+    // 進捗は、そのノードのクラス名：statusXのX部分にあるため
+    var status = form.className.replace("status", "");
+    send(id, "update", pos, add, deadline, status);
+    console.log("send!!");
 }
